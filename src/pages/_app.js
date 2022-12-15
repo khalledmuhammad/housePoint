9f56642a41253b4c3c7bb6001c83c60b4562afef
khaled muhammed <Khaled.Saad@appittuneit.co.uk>
2022-12-15 16:31:36 +0200

    added loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,32 @@ import { store } from "../app/store";
 import ScrollToTop from "../components/common/ScrollTop";
 import Seo from "../components/common/seo";
 import "../index.scss";
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import NProgress from "nprogress";
+
 
 if (typeof window !== "undefined") {
   require("bootstrap/dist/js/bootstrap");
 }
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      NProgress.start();
+    });
+
+    router.events.on('routeChangeComplete', () => {
+      NProgress.done();
+    });
+    return () => {
+      NProgress.done();
+    };
+  }, []);
+
+
   return (
     <>
       <Seo
