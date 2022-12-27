7f007a01cca5971bd4ca46f19cec2f53bbf2fdc6
khaled muhammed <Khaled.Saad@appittuneit.co.uk>
2022-12-27 13:45:08 +0200

    fmf

--- src/components/common/listing-details/PropertyItem.jsx
+++ src/components/common/listing-details/PropertyItem.jsx
@@ -1,24 +1,24 @@
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
 
-const PropertyItem = ({property}) => {
+const PropertyItem = ({Properties}) => {
 
   const router = useRouter()
   const {t} = useTranslation()
   return (
     <ul className="mb0">
-      <li className="list-inline-item">
-        <a href="#">{ property && router.locale === "ar" ? property.type_ar  : property.type_en}</a>
+{/*       <li className="list-inline-item">
+        <a href="#">{ Properties && router.locale === "ar" ? Properties.type_ar  : Properties.type_en}</a>
       </li>
       <li className="list-inline-item">
-        <a href="#">{t("BEDS")}:  { property && property.No_of_bedrooms} </a>
+        <a href="#">{t("BEDS")}:  { Properties && Properties.No_of_bedrooms} </a>
       </li>
       <li className="list-inline-item mx-3">
-        <a href="#">{t("BATHS")}:  { property && property.No_of_bathrooms}  </a>
+        <a href="#">{t("BATHS")}:  { Properties && Properties.No_of_bathrooms}  </a>
       </li>
       <li className="list-inline-item">
-        <a href="#">Sq Ft: { property && property.Surface_area}</a>
-      </li>
+        <a href="#">Sq Ft: { Properties && Properties.Surface_area}</a>
+      </li> */}
     </ul>
   );
 };
